edf3161d97811c5cfb111c92645d72d41a8492d3
Valentine Herrmann <herrmannv@si.edu>
2021-05-21 14:01:13 -0400

    try a different way to test and report issues

--- testthat/test-species.R
+++ testthat/test-species.R
@@ -1,27 +1,9 @@
 library(here)
+library(testthat)
 
-test_that("All species are valid in latest mort census", {
+test_that("Some species codes are not valid", {
   
-  
-  # read latest mortality census survey
-  all_mort_files <- list.files(here("raw_data"), pattern = "Mortality_Survey_.*csv", full.names = T)
-  
-  i <- which.max(as.numeric(substr(all_mort_files, nchar(all_mort_files)-7, nchar(all_mort_files)-4)))
-  
-  mort <- read.csv(all_mort_files[i])
-  
-  
-  # read species table
-  
-  spptable <- read.csv("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/tree_main_census/data/census-csv-files/scbi.spptable.csv")
-  
-  # check if all species exist in species table
-  
-  if(!all(mort[,grepl("^sp", names(mort))] %in% spptable$sp)) {
-    write.csv(mort[!mort[,grepl("^sp", names(mort))] %in% spptable$sp,], file = file.path(here("testthat"), "reports/species_code_error.csv"), row.names = F)
-  } else {
-    if(file.exists(file.path(here("testthat"), "reports/species_code_error.csv")) ) file.remove(file.path(here("testthat"), "reports/species_code_error.csv"))
-  }
-  
-  expect_true(all(mort[,grepl("^sp", names(mort))] %in% spptable$sp))
+  species_report_exists  <-  ifelse(file.exists(file.path(here("testthat"), "reports/species_code_error.csv")), TRUE, FALSE)
+
+  expect_false(species_report_exists)
 })
