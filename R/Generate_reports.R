b4f7ec9fa271c63936eb327747469454e4c43dfe
Valentine Herrmann <herrmannv@si.edu>
2021-05-21 14:00:28 -0400

    rename R script folder

---
+++ R/Generate_reports.R
@@ -0,0 +1,25 @@
+# Generate reports looking at latest mortality census raw data ####
+## this script is run automatically when there is a push 
+
+library(here)
+
+# read latest mortality census survey
+  all_mort_files <- list.files(here("raw_data"), pattern = "Mortality_Survey_.*csv", full.names = T)
+  
+  i <- which.max(as.numeric(substr(all_mort_files, nchar(all_mort_files)-7, nchar(all_mort_files)-4)))
+  
+  mort <- read.csv(all_mort_files[i])
+  
+  
+  # read species table
+  
+  spptable <- read.csv("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/tree_main_census/data/census-csv-files/scbi.spptable.csv")
+  
+  # check if all species exist in species table
+  
+  if(!all(mort[,grepl("^sp", names(mort))] %in% spptable$sp)) {
+    write.csv(mort[!mort[,grepl("^sp", names(mort))] %in% spptable$sp,], file = file.path(here("testthat"), "reports/species_code_error.csv"), row.names = F)
+  } else {
+    if(file.exists(file.path(here("testthat"), "reports/species_code_error.csv")) ) file.remove(file.path(here("testthat"), "reports/species_code_error.csv"))
+  }
+  
