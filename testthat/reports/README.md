c38fe8f9c842cb9120bcf57d49788fd987a8a905
Valentine Herrmann <herrmannv@si.edu>
2021-05-21 13:25:52 -0400

    Saving error reports if a species does not exist in species list

---
+++ testthat/reports/README.md
@@ -0,0 +1,3 @@
+# Error Reports
+
+This folder contains subsets of the latest moratlity census with data entry errors that need to be fixed.
--- testthat/test-species.R
+++ testthat/test-species.R
@@ -17,5 +17,11 @@ test_that("All species are valid in latest mort census", {
   
   # check if all species exist in species table
   
+  if(!all(mort[,grepl("^sp", names(mort))] %in% spptable$sp)) {
+    write.csv(mort[!mort[,grepl("^sp", names(mort))] %in% spptable$sp,], file = "testthat/reports/species_code_error.csv", row.names = F)
+  } else {
+    file.remove("testthat/reports/species_code_error.csv")
+  }
+  
   expect_true(all(mort[,grepl("^sp", names(mort))] %in% spptable$sp))
 })
