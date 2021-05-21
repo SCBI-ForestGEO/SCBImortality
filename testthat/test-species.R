479fb89941f5c302ab082edfc22fbd26326e5c45
Valentine Herrmann <herrmannv@si.edu>
2021-05-21 15:13:35 -0400

    add test to check if all stems were censused in quadrat already processed #2

--- testthat/test-species.R
+++ testthat/test-species.R
@@ -7,3 +7,11 @@ test_that("Some species codes are not valid", {
 
   expect_false(species_report_exists)
 })
+
+
+test_that("Some stems were missed in a quadrat already censused", {
+  
+  species_report_exists  <-  ifelse(file.exists(file.path(here("testthat"), "reports/quadrat_censused_missing_stems.csv")), TRUE, FALSE)
+  
+  expect_false(species_report_exists)
+})
