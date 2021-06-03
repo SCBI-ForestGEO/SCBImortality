15882056305d17c7985d03bbb5aed1a57ef9e7d3
Valentine Herrmann <herrmannv@si.edu>
2021-06-03 14:33:30 -0400

    minor fix for consistency

--- testthat/test-species.R
+++ testthat/test-species.R
@@ -3,7 +3,7 @@ library(testthat)
 
 test_that("Some species codes are not valid", {
   
-  species_report_exists  <-  ifelse(file.exists(file.path(here("testthat"), "reports/species_code_error.csv")), TRUE, FALSE)
+  report_exists  <-  ifelse(file.exists(file.path(here("testthat"), "reports/species_code_error.csv")), TRUE, FALSE)
 
-  expect_false(species_report_exists)
+  expect_false(report_exists)
 })
