f72c69975d7c6fa849651871fa8fe2745b1c0aff
Valentine Herrmann <herrmannv@si.edu>
2021-07-01 18:15:38 -0400

    Update test-wounded-and-level-selected.R

--- testthat/test-wounded-and-level-selected.R
+++ testthat/test-wounded-and-level-selected.R
@@ -3,7 +3,7 @@ library(testthat)
 
 test_that("Some trees are wounded but have no Wounded main stem selected or vice versa", {
   
-  report_exists  <-  any(ifelse(file.exists(file.path(here("testthat"), "reports/requires_field_fixwounded_but_no_level.csv")), TRUE, FALSE))
+  report_exists  <-  any(ifelse(file.exists(file.path(here("testthat"), "reports/requires_field_fix/wounded_but_no_level.csv")), TRUE, FALSE))
   
   expect_false(report_exists)
 })
