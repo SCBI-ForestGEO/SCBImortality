e59f928bd5a05029fc363a4d3a4cfe7a868eb30e
rudeboybert <albert.ys.kim@gmail.com>
2021-07-07 10:03:16 -0400

    catching NULL & length 0 cases

--- R_scripts/Generate_warnings.R
+++ R_scripts/Generate_warnings.R
@@ -10,13 +10,13 @@ library(here)
 
 # load files ####
 
-will_auto_fix_error_file <- file.path(here("testthat"), "reports/will_auto_fix/will_auto_fix_error_file.csv")
-if(file.exists(will_auto_fix_error_file)) will_auto_fix_error_file <- read.csv(will_auto_fix_error_file)
+will_auto_fix_error_filepath <- file.path(here("testthat"), "reports/will_auto_fix/will_auto_fix_error_file.csv")
+if(file.exists(will_auto_fix_error_filepath)) will_auto_fix_error_file <- read.csv(will_auto_fix_error_filepath)
 
 
 
-warning_file <- file.path(here("testthat"), "reports/warnings/warnings_file.csv")
-if(file.exists(warning_file)) warning_file <- read.csv(warning_file)
+warning_filepath <- file.path(here("testthat"), "reports/warnings/warnings_file.csv")
+if(file.exists(warning_filepath)) warning_file <- read.csv(warning_filepath)
 
 
 # write warning messages ####
@@ -34,9 +34,10 @@ warning_messages <- c("unhealthy_but_wrong_status" = "There are living tress tha
 
 # check if files exist and generate a plot with the warning ####
 
+all_will_be_fixed <- NULL
 if(exists("will_auto_fix_error_file")) all_will_be_fixed <- warning_messages[unique(will_auto_fix_error_file$error_name)]
 
-
+all_warns <- NULL
 if(exists("warning_file")) all_warns <- warning_messages[unique(warning_file$warning_name)]
 
 
