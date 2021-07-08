dec8b966ec1b61998bdd361dc360a7df47380016
Valentine Herrmann <herrmannv@si.edu>
2021-07-08 11:23:48 -0400

    implementing fixes @rudeboybert found
    @rudeboybert, thanks for your pull request https://github.com/SCBI-ForestGEO/SCBImortality/pull/27. I implemented your fixes in the main branch as there were some conflicts to be resolved. I think the pull request (and branch) can be deleted now #27

--- R_scripts/Generate_reports.R
+++ R_scripts/Generate_reports.R
@@ -579,10 +579,13 @@ warning_file <- warning_file[order(warning_file$Quad, warning_file$Tag, warning_
 
 
 # save
+if(!is.null(require_field_fix_error_file))
 write.csv(require_field_fix_error_file[, c(ncol(require_field_fix_error_file), 1:(ncol(require_field_fix_error_file) -1))], file = file.path(here("testthat"), "reports/requires_field_fix/require_field_fix_error_file.csv"), row.names = F)
 
+if(!is.null(will_auto_fix_error_file))
 write.csv(will_auto_fix_error_file[, c(ncol(will_auto_fix_error_file), 1:(ncol(will_auto_fix_error_file) -1))], file = file.path(here("testthat"), "reports/will_auto_fix/will_auto_fix_error_file.csv"), row.names = F)
 
+if(!is.null(warning_file))
 write.csv(warning_file[, c(ncol(warning_file), 1:(ncol(warning_file) -1))], file = file.path(here("testthat"), "reports/warnings/warnings_file.csv"), row.names = F)
 
 
--- R_scripts/Generate_warnings.R
+++ R_scripts/Generate_warnings.R
@@ -10,13 +10,13 @@ library(here)
 
 # load files ####
 
-will_auto_fix_error_file <- file.path(here("testthat"), "reports/will_auto_fix/will_auto_fix_error_file.csv")
-if(file.exists(will_auto_fix_error_file)) will_auto_fix_error_file <- read.csv(will_auto_fix_error_file)
+will_auto_fix_error_file_path <- file.path(here("testthat"), "reports/will_auto_fix/will_auto_fix_error_file.csv")
+if(file.exists(will_auto_fix_error_file_path)) will_auto_fix_error_file <- read.csv(will_auto_fix_error_file_path)
 
 
 
-warning_file <- file.path(here("testthat"), "reports/warnings/warnings_file.csv")
-if(file.exists(warning_file)) warning_file <- read.csv(warning_file)
+warning_file_path <- file.path(here("testthat"), "reports/warnings/warnings_file.csv")
+if(file.exists(warning_file_path)) warning_file <- read.csv(warning_file_path)
 
 
 # write warning messages ####
@@ -34,10 +34,10 @@ warning_messages <- c("unhealthy_but_wrong_status" = "There are living tress tha
 
 # check if files exist and generate a plot with the warning ####
 
-if(exists("will_auto_fix_error_file")) all_will_be_fixed <- warning_messages[unique(will_auto_fix_error_file$error_name)]
+if(exists("will_auto_fix_error_file")) all_will_be_fixed <- paste(c("ERRORS THAT WILL AUTO FIX:\n", warning_messages[unique(will_auto_fix_error_file$error_name)]), collapse = "\n") else  all_will_be_fixed <- ""
 
 
-if(exists("warning_file")) all_warns <- warning_messages[unique(warning_file$warning_name)]
+if(exists("warning_file")) all_warns <- paste(c("WARNINGS!!!\n", warning_messages[unique(warning_file$warning_name)], "CLICK HERE TO GO TO FOLDER"), collapse = "\n") else all_warns = "No WARNINGS"
 
 
 filename <- file.path(here("testthat"), "reports/warnings.png")
@@ -45,12 +45,11 @@ filename <- file.path(here("testthat"), "reports/warnings.png")
 
 if(length(all_warns) == 0 & length(all_will_be_fixed) == 0)  file.remove(filename)
 
-png(filename, width = 6, height = 1 + (0.2*length(c(all_warns, all_will_be_fixed))), units = "in", res = 300)
+png(filename, width = 6, height = 0.7 + (0.2*length(c(gregexpr("\\n", all_warns)[[1]], gregexpr("\\n", all_will_be_fixed)[[1]]))), units = "in", res = 300)
 par(mar = c(0,0,0,0))
 plot(0,0, axes = F, xlab = "", ylab = "", type = "n")
-text(0,0.7, paste(c("WARNINGS!!!\n", all_warns, "CLICK HERE TO GO TO FOLDER"), collapse = "\n"), col = "red", cex = 0.6)
-
-text(0,0, paste(c("ERRORS THAT WILL AUTO FIX:\n", all_will_be_fixed), collapse = "\n"), col = "red", cex = 0.6, pos = 1)
+text(0,0.7, all_warns, col = "red", cex = 0.6)
+text(0,0, all_will_be_fixed, col = "red", cex = 0.6, pos = 1)
 # title("warnings!!!", col.main= "red", xpd = NULL, line = -1)
 dev.off()
 
