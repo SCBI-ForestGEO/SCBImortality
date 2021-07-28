0e2bc24098b73a068ec75507dbf2c345b6df38a0
rudeboybert <albert.ys.kim@gmail.com>
2021-07-28 12:19:19 -0400

    Update daily progress plot

--- R_scripts/Generate_daily_progress_reports.R
+++ R_scripts/Generate_daily_progress_reports.R
@@ -14,24 +14,51 @@ library(ggplot2)
 
 
 
-# Load quadrat info and censused stem counts --------------------------------
+# Load census stem counts --------------------------------
+# Get stem counts from main census (copied from Generate_reports.R)
+main_census <-  read.csv(paste0("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/tree_main_census/data/census-csv-files/scbi.stem3.csv"))
+
+## convert dbh to numeric
+main_census$dbh <- as.numeric(main_census$dbh)
+
+## only keep trees > 10cm except for fraxinus and Chionanthus virginicus
+main_census <-  main_census[grepl("^fr..|^ch..", main_census$sp) | (!is.na(main_census$dbh) & main_census$dbh >= 100), ]
+
+## remove trees that are dead
+# main_census <- main_census[!main_census$status %in% "D",]
+main_census <- main_census[!grepl("DC|DN|DT", main_census$codes),] 
+
+census_quad_counts <- main_census %>% 
+  group_by(quadrat) %>% 
+  summarize(n_census = n()) %>% 
+  rename(quad = quadrat) %>% 
+  mutate(quad = str_pad(quad, width = 4, side = "left", pad = "0"))
+
+
+# Load quadrat info --------------------------------
 # Get latest FFF
 latest_FFFs <- list.files(here("raw_data/FFF_excel/"), pattern = ".xlsx", full.names = T)
 
 # Get info on all quadrats censused so far
 quadrat_info <- read_xlsx(latest_FFFs, sheet = "Root", .name_repair = "minimal" ) %>% 
   clean_names() %>% 
-  select(submission_id, quad, date_time, personnel, stem_count) %>% 
+  select(-submitted_by, -starts_with("form")) %>% 
   mutate(
     stem_count = as.numeric(stem_count),
-    date_time = mdy(date_time)
-  )
+    date_time = mdy(date_time),
+    submitted_on = parse_date_time(submitted_on, "m-d-Y H:M p"),
+    quad = str_pad(quad, width = 4, side = "left", pad = "0")
+  ) %>% 
+  arrange(date_time) %>% 
+  # Stem counts from FFF were built off wrong census file, so replace
+  # census stem counts from above
+  left_join(census_quad_counts, by = "quad") %>% 
+  select(-stem_count) %>% 
+  rename(stem_count = n_census)
 
-# Get number of stems censused per day
 stems_censused_per_day <- quadrat_info %>% 
   group_by(date_time) %>% 
   summarize(n_stems = sum(stem_count, na.rm=TRUE))
-
 
 
 # Load trace of error reports ---------------------------------------
@@ -59,17 +86,6 @@ auto_fix_errors <- read_csv("testthat/reports/trace_of_reports/will_auto_fix_err
   group_by(date_time) %>% 
   summarize(auto_fix = n()) 
 
-# Missing stems
-quadrat_censused_missing_stems <- read_csv("testthat/reports/trace_of_reports/quadrat_censused_missing_stems.csv") %>% 
-  clean_names() %>% 
-  mutate(
-    quad = str_pad(quadrat, 4, "left", "0"),
-    date_time = mdy(exact_date)
-  ) %>% 
-  # Determine level of aggregation:
-  group_by(date_time) %>% 
-  summarize(missing_stems = n()) 
-
 # Duplicated stems
 quadrat_censused_duplicated_stems <- read_csv("testthat/reports/trace_of_reports/quadrat_censused_duplicated_stems.csv") %>% 
   clean_names() %>% 
@@ -93,7 +109,7 @@ error_rates <-
   field_fix_errors %>% 
   left_join(auto_fix_errors, by = "date_time") %>% 
   left_join(quadrat_censused_duplicated_stems, by = "date_time") %>% 
-  left_join(quadrat_censused_missing_stems, by = "date_time") %>% 
+  filter(!is.na(date_time)) %>% 
   replace(is.na(.), 0) %>%
   pivot_longer(cols = -c(date_time), names_to = "error_type", values_to = "n_errors") %>% 
   # Get error rate being sure to account for days where 0 errors occurred
@@ -115,7 +131,9 @@ census_rate_plot <- stems_censused_per_day %>%
   labs(
     x = "Census date", y = "# of new stems censused",
     title = "Daily new stems censused"
-  )
+  )+
+  geom_vline(xintercept = ymd("2021-07-06"), col = "red") +
+  theme_bw()
 
 error_rate_plot <- error_rates %>%
   ggplot(aes(x=date_time, y = errors_per_stem, col = error_type)) +
@@ -129,12 +147,12 @@ error_rate_plot <- error_rates %>%
   coord_cartesian(
     ylim = c(0, NA)
   ) +
-  theme(legend.position="bottom")
+  theme(legend.position="bottom") +
+  geom_vline(xintercept = ymd("2021-07-06"), col = "red") +
+  theme_bw()
 
-# Merge ggplots using patchwork
-progress_plot <- census_rate_plot /
-  error_rate_plot 
 
 # Output
 filename <- file.path(here("testthat"), "reports/daily_progress.png")
-ggsave(filename, plot= progress_plot, device = 'png', width = 16/2, height = 18/2.5, units = "in",dpi = 300 )
+census_rate_plot / error_rate_plot 
+ggsave(filename, device = 'png', width = 16/2, height = 18/2.5, units = "in",dpi = 300 )
