3e2e4ba124a0a81f497c61e932cc7157a8040a2c
Herrmann <HerrmannV@SI.EDU>
2022-04-20 12:37:56 -0400

    bring back AtoAU analysis #68
    @teixeirak, FYI

--- R_scripts/2_Calculate_mortality_and_AtoAU_rates.R
+++ R_scripts/2_Calculate_mortality_and_AtoAU_rates.R
@@ -41,7 +41,7 @@ allmort <- allmort[!is.na(allmort$last_main_census_dbh) & allmort$last_main_cens
 allmort$died <- grepl("A", allmort$previous_year_status) & grepl("D", allmort$current_year_status)
 
 # ## did tree went from A to AU between now and previous census ?
-# allmort$AtoAU <- grepl("A$", allmort$previous_year_status) & grepl("AU", allmort$current_year_status)
+allmort$AtoAU <- grepl("A$", allmort$previous_year_status) & grepl("AU", allmort$current_year_status)
 
 ## time interval between censused, in year
 allmort$timeint <- allmort$timeint_days / 365.25
@@ -67,7 +67,7 @@ ninit <- tapply(mort$previous_year_status, mort[, c("survey_year", "sp")], funct
 ndead <- tapply(mort$died, mort[, c("survey_year", "sp")], sum, na.rm = T)
 
 # ## get number of unhealthy trees per species and year
-# nAtoAU <- tapply(mort$AtoAU, mort[, c("survey_year", "sp")], sum, na.rm = T)
+nAtoAU <- tapply(mort$AtoAU, mort[, c("survey_year", "sp")], sum, na.rm = T)
 
 
 ## get mean time interval per species and year
@@ -77,13 +77,13 @@ mntime <- tapply(mort$timeint, mort[, c("survey_year", "sp")], mean, na.rm = T)
 mortrate <- 100 * (1 - ((ninit - ndead) / ninit) ^ (1 / mntime))
 
 # ## calculate rate of health decline per species and year
-# AtoAUrate <- 100 * (1 - ((ninit - nAtoAU) / ninit) ^ (1 / mntime))
+AtoAUrate <- 100 * (1 - ((ninit - nAtoAU) / ninit) ^ (1 / mntime))
 
 ## get biomass mortality
 agbmort <-  tapply(mort$agb_yr[mort$died], mort[mort$died, c("survey_year", "sp")], sum)/hectar
   
 # ## get biomass health decline
-# agbAtoAU <-  tapply(mort$agb_yr[mort$AtoAU], mort[mort$AtoAU, c("survey_year", "sp")], sum)/hectar
+agbAtoAU <-  tapply(mort$agb_yr[mort$AtoAU], mort[mort$AtoAU, c("survey_year", "sp")], sum)/hectar
 
 
 ### bootstrap to get CI ####
@@ -101,8 +101,8 @@ dsums[is.na(dsums)] <- 0
 msums <- tapply(mort$died, mort[, c("survey_year", "sp", "rowcol")], sum, na.rm = T)
 msums[is.na(msums)] <- 0
 
-# atoausums<- tapply(mort$AtoAU, mort[, c("survey_year", "sp", "rowcol")], sum, na.rm = T)
-# atoausums[is.na(atoausums)] <- 0
+atoausums<- tapply(mort$AtoAU, mort[, c("survey_year", "sp", "rowcol")], sum, na.rm = T)
+atoausums[is.na(atoausums)] <- 0
 
 ### sum the time interval in each grid and size class
 tsums <-  tapply(mort$timeint, mort[, c("survey_year", "sp", "rowcol")], sum, na.rm = T)
@@ -112,8 +112,8 @@ tsums[is.na(tsums)] <- 0
 magbsum <- tapply(mort$agb_yr[mort$died], mort[mort$died, c("survey_year", "sp", "rowcol")], sum, na.rm = T)/hectar
 magbsum[is.na(magbsum)] <- 0
 
-# auagbsum <- tapply(mort$agb_yr[mort$AtoAU], mort[mort$AtoAU, c("survey_year", "sp", "rowcol")], sum, na.rm = T)/hectar
-# auagbsum[is.na(auagbsum)] <- 0
+auagbsum <- tapply(mort$agb_yr[mort$AtoAU], mort[mort$AtoAU, c("survey_year", "sp", "rowcol")], sum, na.rm = T)/hectar
+auagbsum[is.na(auagbsum)] <- 0
 
 ### get number of grid
 ngrid = dim(dn)[3]
@@ -122,8 +122,8 @@ ngrid = dim(dn)[3]
 boot_mort <- boot_agbmort <- array(NA, dim = c(dim(dn)[1:2], nreps), 
                    dimnames = c(dimnames(dn)[1:2], list(rep_id = 1:nreps)))
 
-# boot_AtoAU <- boot_agbAtoAU <- array(NA, dim = c(dim(dn)[1:2], nreps), 
-#                                    dimnames = c(dimnames(dn)[1:2], list(rep_id = 1:nreps)))
+boot_AtoAU <- boot_agbAtoAU <- array(NA, dim = c(dim(dn)[1:2], nreps),
+                                   dimnames = c(dimnames(dn)[1:2], list(rep_id = 1:nreps)))
 
 pb <- progress_bar$new(
   format = paste("Calculating 95%CI [:bar] :percent eta: :eta"),
@@ -142,10 +142,10 @@ for(i in 1:nreps) {
   
   boot_agbmort[,,i]= apply(magbsum[,,whichgrids], c(1,2), sum, na.rm = T)
   
-  # boot_AtoAU[,,i] = 100 * (1 - ((apply(dn[,,whichgrids], c(1,2), sum, na.rm = T) - apply(atoausums[,,whichgrids], c(1,2), sum, na.rm =T)) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T)) ^ (1 / (apply(tsums[,,whichgrids], c(1,2), sum, na.rm = T) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T))))
-  # 
-  # boot_agbAtoAU[,,i]= apply(auagbsum[,,whichgrids], c(1,2), sum, na.rm = T)
-  # 
+  boot_AtoAU[,,i] = 100 * (1 - ((apply(dn[,,whichgrids], c(1,2), sum, na.rm = T) - apply(atoausums[,,whichgrids], c(1,2), sum, na.rm =T)) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T)) ^ (1 / (apply(tsums[,,whichgrids], c(1,2), sum, na.rm = T) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T))))
+
+  boot_agbAtoAU[,,i]= apply(auagbsum[,,whichgrids], c(1,2), sum, na.rm = T)
+
   
   ## get average timeint
   # apply(tsums[,,whichgrids], c(1,2), sum, na.rm = T) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T) 
@@ -157,10 +157,10 @@ abgmort_lci <- apply(boot_agbmort, c(1,2), quantile, probs = 0.025, na.rm = T)
 abgmort_uci <- apply(boot_agbmort, c(1,2), quantile, probs = 0.975, na.rm = T)
 
 
-# AtoAU_lci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.025, na.rm = T)
-# AtoAU_uci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.975, na.rm = T)
-# agbAtoAU_lci <- apply(boot_agbAtoAU, c(1,2), quantile, probs = 0.025, na.rm = T)
-# agbAtoAU_uci <- apply(boot_agbAtoAU, c(1,2), quantile, probs = 0.975, na.rm = T)
+AtoAU_lci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.025, na.rm = T)
+AtoAU_uci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.975, na.rm = T)
+agbAtoAU_lci <- apply(boot_agbAtoAU, c(1,2), quantile, probs = 0.025, na.rm = T)
+agbAtoAU_uci <- apply(boot_agbAtoAU, c(1,2), quantile, probs = 0.975, na.rm = T)
 
 
 ### put everything together ####
@@ -185,36 +185,36 @@ sp_mortrate_agbmort <- sp_mortrate_agbmort[!is.na(sp_mortrate_agbmort$estimate),
 
 
 
-# sp_AtoAUrate <- cbind(variable = "mortality_rates", as.data.frame.table(AtoAUrate, responseName = "estimate"))
-# sp_AtoAUrate$LCI_95 <- as.data.frame.table(AtoAU_lci)$Freq
-# sp_AtoAUrate$UCI_95 <- as.data.frame.table(AtoAU_uci)$Freq
-# sp_AtoAUrate$N0 <- as.data.frame.table(ninit)$Freq
-# sp_AtoAUrate$n_died<- as.data.frame.table(nAtoAU)$Freq
-# sp_AtoAUrate$mean_timeint_yr<- as.data.frame.table(mntime)$Freq
-# 
-# 
-# sp_agbAtoAU <- cbind(variable = "biomass_mortality", as.data.frame.table(agbAtoAU, responseName = "estimate"))
-# sp_agbAtoAU$LCI_95 <- as.data.frame.table(agbAtoAU_lci)$Freq
-# sp_agbAtoAU$UCI_95 <- as.data.frame.table(agbAtoAU_uci)$Freq
-# sp_agbAtoAU$N0 <- as.data.frame.table(ninit)$Freq
-# sp_agbAtoAU$n_died<- as.data.frame.table(nAtoAU)$Freq
-# sp_agbAtoAU$mean_timeint_yr<- as.data.frame.table(mntime)$Freq
-# 
-# 
-# sp_AtoAUrate_agbAtoAU <- rbind(sp_AtoAUrate, sp_agbAtoAU)
-# sp_AtoAUrate_agbAtoAU <- sp_AtoAUrate_agbAtoAU[!is.na(sp_AtoAUrate_agbAtoAU$estimate), ]
+sp_AtoAUrate <- cbind(variable = "mortality_rates", as.data.frame.table(AtoAUrate, responseName = "estimate"))
+sp_AtoAUrate$LCI_95 <- as.data.frame.table(AtoAU_lci)$Freq
+sp_AtoAUrate$UCI_95 <- as.data.frame.table(AtoAU_uci)$Freq
+sp_AtoAUrate$N0 <- as.data.frame.table(ninit)$Freq
+sp_AtoAUrate$n_died<- as.data.frame.table(nAtoAU)$Freq
+sp_AtoAUrate$mean_timeint_yr<- as.data.frame.table(mntime)$Freq
+
+
+sp_agbAtoAU <- cbind(variable = "biomass_mortality", as.data.frame.table(agbAtoAU, responseName = "estimate"))
+sp_agbAtoAU$LCI_95 <- as.data.frame.table(agbAtoAU_lci)$Freq
+sp_agbAtoAU$UCI_95 <- as.data.frame.table(agbAtoAU_uci)$Freq
+sp_agbAtoAU$N0 <- as.data.frame.table(ninit)$Freq
+sp_agbAtoAU$n_died<- as.data.frame.table(nAtoAU)$Freq
+sp_agbAtoAU$mean_timeint_yr<- as.data.frame.table(mntime)$Freq
+
+
+sp_AtoAUrate_agbAtoAU <- rbind(sp_AtoAUrate, sp_agbAtoAU)
+sp_AtoAUrate_agbAtoAU <- sp_AtoAUrate_agbAtoAU[!is.na(sp_AtoAUrate_agbAtoAU$estimate), ]
 
 
 # save ####
 write.csv(sp_mortrate_agbmort, file = "R_results/mortrates_and_agbmort_by_species.csv", row.names = F)
-# write.csv(sp_AtoAUrate_agbAtoAU, file = "R_results/AtoAUrates_and_agbAtoAU_by_species.csv", row.names = F)
+write.csv(sp_AtoAUrate_agbAtoAU, file = "R_results/AtoAUrates_and_agbAtoAU_by_species.csv", row.names = F)
 
 
 
 #### Calculate mortality rates size classes ####
 
 all_mortrate <- NULL
-# all_AtoAUrate <- NULL
+all_AtoAUrate <- NULL
 
 for(sp in c("All", sort(unique(as.character(allmort$sp))))) {
 
@@ -229,14 +229,14 @@ ninit <- tapply(mort$previous_year_status, mort[, c("survey_year", "dbh_class")]
 
 ## get number of dead (and AtoAU) trees per size class ####
 ndead <- tapply(mort$died, mort[, c("survey_year", "dbh_class")], sum, na.rm = T)
-# nAtoAU <-  tapply(mort$AtoAU, mort[, c("survey_year", "dbh_class")], sum, na.rm = T)
+nAtoAU <-  tapply(mort$AtoAU, mort[, c("survey_year", "dbh_class")], sum, na.rm = T)
 
 ## get mean time interval per size class ####
 mntime <- tapply(mort$timeint, mort[, c("survey_year", "dbh_class")], mean, na.rm = T)
 
 ## calculate mortality (and AtuAU ) rates per size class ####
 mortrate <- 100 * (1 - ((ninit - ndead) / ninit) ^ (1 / mntime))
-# AtoAUrate <- 100 * (1 - ((ninit - nAtoAU) / ninit) ^ (1 / mntime))
+AtoAUrate <- 100 * (1 - ((ninit - nAtoAU) / ninit) ^ (1 / mntime))
 
 
 ### bootstrap to get CI ####
@@ -254,8 +254,8 @@ dsums[is.na(dsums)] <- 0
 msums <- tapply(mort$died, mort[, c("survey_year", "dbh_class", "rowcol")], sum, na.rm = T)
 msums[is.na(msums)] <- 0
 
-# atoausums<- tapply(mort$AtoAU, mort[, c("survey_year", "dbh_class", "rowcol")], sum, na.rm = T)
-# atoausums[is.na(atoausums)] <- 0
+atoausums<- tapply(mort$AtoAU, mort[, c("survey_year", "dbh_class", "rowcol")], sum, na.rm = T)
+atoausums[is.na(atoausums)] <- 0
 
 ### sum the time interval in each grid and size class
 tsums <-  tapply(mort$timeint, mort[, c("survey_year", "dbh_class", "rowcol")], sum, na.rm = T)
@@ -265,7 +265,7 @@ tsums[is.na(tsums)] <- 0
 ngrid = dim(dn)[3]
 
 ## bootstrap the grids (sample ngrid with replacement) ####
-boot_mort <- array(NA, dim = c(dim(dn)[1:2], nreps), 
+boot_mort <- boot_AtoAU <- array(NA, dim = c(dim(dn)[1:2], nreps), 
                                  dimnames = c(dimnames(dn)[1:2], list(rep_id = 1:nreps)))
 
 pb <- progress_bar$new(
@@ -283,7 +283,8 @@ for(i in 1:nreps) {
   ###  100 * (1 - ((ninit - ndead) / ninit) ^ (1 / mntime))
   boot_mort[,,i] = 100 * (1 - ((apply(dn[,,whichgrids], c(1,2), sum, na.rm = T) - apply(msums[,,whichgrids], c(1,2), sum, na.rm =T)) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T)) ^ (1 / (apply(tsums[,,whichgrids], c(1,2), sum, na.rm = T) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T))))
   
-  # boot_AtoAU[,,i] = 100 * (1 - ((apply(dn[,,whichgrids], c(1,2), sum, na.rm = T) - apply(atoausums[,,whichgrids], c(1,2), sum, na.rm =T)) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T)) ^ (1 / (apply(tsums[,,whichgrids], c(1,2), sum, na.rm = T) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T))))
+  boot_AtoAU[,,i] = 100 * (1 - ((apply(dn[,,whichgrids], c(1,2), sum, na.rm = T) - apply(atoausums[,,whichgrids], c(1,2), sum, na.rm =T)) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T)) ^ (1 / (apply(tsums[,,whichgrids], c(1,2), sum, na.rm = T) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T))))
+  
   ## get average timeint
   # apply(tsums[,,whichgrids], c(1,2), sum, na.rm = T) / apply(dn[,,whichgrids],  c(1,2), sum, na.rm = T) 
 }
@@ -291,8 +292,8 @@ for(i in 1:nreps) {
 mort_lci <- apply(boot_mort, c(1,2), quantile, probs = 0.025, na.rm = T)
 mort_uci <- apply(boot_mort, c(1,2), quantile, probs = 0.975, na.rm = T)
 
-# AtoAU_lci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.025, na.rm = T)
-# AtoAU_uci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.975, na.rm = T)
+AtoAU_lci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.025, na.rm = T)
+AtoAU_uci <- apply(boot_AtoAU, c(1,2), quantile, probs = 0.975, na.rm = T)
 
 
 # put everything together ####
@@ -305,26 +306,26 @@ sp_mortrate$n_died<- as.data.frame.table(ndead)$Freq
 sp_mortrate$mean_timeint_yr<- as.data.frame.table(mntime)$Freq
 
 
-# sp_AtoAUrate <-  cbind(Species = sp, as.data.frame.table(AtoAUrate, responseName = "AtoAU_rate"))
-# sp_AtoAUrate$LCI_95 <- as.data.frame.table(AtoAU_lci)$Freq
-# sp_AtoAUrate$UCI_95 <- as.data.frame.table(mort_uci)$Freq
-# sp_AtoAUrate$mean_dbh <- as.data.frame.table(AtoAU_uci)$Freq
-# sp_AtoAUrate$N0 <- as.data.frame.table(ninit)$Freq
-# sp_AtoAUrate$n_died<- as.data.frame.table(nAtoAU)$Freq
-# sp_AtoAUrate$mean_timeint_yr<- as.data.frame.table(mntime)$Freq
+sp_AtoAUrate <-  cbind(Species = sp, as.data.frame.table(AtoAUrate, responseName = "AtoAU_rate"))
+sp_AtoAUrate$LCI_95 <- as.data.frame.table(AtoAU_lci)$Freq
+sp_AtoAUrate$UCI_95 <- as.data.frame.table(mort_uci)$Freq
+sp_AtoAUrate$mean_dbh <- as.data.frame.table(AtoAU_uci)$Freq
+sp_AtoAUrate$N0 <- as.data.frame.table(ninit)$Freq
+sp_AtoAUrate$n_died<- as.data.frame.table(nAtoAU)$Freq
+sp_AtoAUrate$mean_timeint_yr<- as.data.frame.table(mntime)$Freq
 
 
 
 all_mortrate <- rbind(all_mortrate, sp_mortrate)
-# all_AtoAUrate <- rbind(all_AtoAUrate, sp_AtoAUrate)
+all_AtoAUrate <- rbind(all_AtoAUrate, sp_AtoAUrate)
 
 }
 
 
 # remove NAs ####
 all_mortrate <- all_mortrate[!is.na(all_mortrate$mortality_rate), ]
-# all_AtoAUrate <-  all_AtoAUrate[!is.na(all_AtoAUrate$AtoAU_rate), ]
+all_AtoAUrate <-  all_AtoAUrate[!is.na(all_AtoAUrate$AtoAU_rate), ]
 
 # save ####
 write.csv(all_mortrate, file = "R_results/mortrates_by_size_class.csv", row.names = F)
-# write.csv(all_AtoAUrate, file = "R_results/AtoAUrate_by_size_class.csv", row.names = F)
+write.csv(all_AtoAUrate, file = "R_results/AtoAUrate_by_size_class.csv", row.names = F)
